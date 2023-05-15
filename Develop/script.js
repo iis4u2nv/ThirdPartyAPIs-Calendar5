cd08c865d99ce402d4df3fc21fbd3627cfa8f381
amazing grace <agracenotary@gmail.com>
2023-05-15 17:53:05 -0400

    added more code to make the page work

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,63 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
+// the code isn't running until the browser has finished rendering all the elements
 // in the html.
+
+// Display today's day and date
 var currentDay = dayjs().format('MMM D, YYYY');
-$(document).ready(
-    function () {
-        $("#currentDay").text(dayjs().format('MMM D, YYYY'))
+$(document).ready(function () {
+
+ $("#currentDay").text(dayjs().format('MMM D, YYYY'))        
+    // saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+        // Get nearby values of the description in JQuery
+        var text = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        
+        // Save text in local storage
+        localStorage.setItem(time, text);
+    })
+           
+    function timeTracker() {
+    //get current number of hours.
+        var timeNow = moment().hour();
+        
+        // loop over time blocks
+        $(".time-block").each(function () {
+        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+        
+        // To check the time and add the classes for background indicators
+        if (blockTime < timeNow) {
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+        $(this).addClass("past");
+        }
+        else if (blockTime === timeNow) {
+        $(this).removeClass("past");
+        $(this).removeClass("future");
+        $(this).addClass("present");
+        }
+        else {
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
+        
+            }
+        })
+    }
+        
+        // Get item from local storage if any
+        $("#hour8 .description").val(localStorage.getItem("hour8"));
+        $("#hour9 .description").val(localStorage.getItem("hour9"));
+        $("#hour10 .description").val(localStorage.getItem("hour10"));
+        $("#hour11 .description").val(localStorage.getItem("hour11"));
+        $("#hour12 .description").val(localStorage.getItem("hour12"));
+        $("#hour13 .description").val(localStorage.getItem("hour13"));
+        $("#hour14 .description").val(localStorage.getItem("hour14"));
+        $("#hour15 .description").val(localStorage.getItem("hour15"));
+        $("#hour16 .description").val(localStorage.getItem("hour16"));
+        $("#hour17 .description").val(localStorage.getItem("hour17"));
+        
+        timeTracker();
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -26,4 +79,4 @@ $(document).ready(
     // TODO: Add code to display the current date in the header of the page.
 
     
-  });
+  })
