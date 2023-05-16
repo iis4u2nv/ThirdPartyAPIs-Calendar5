bef8da462ae1d845d37caef864d31389783d18ff
amazing grace <agracenotary@gmail.com>
2023-05-16 17:37:15 -0400

    added more to make it work

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,7 @@
 // the code isn't running until the browser has finished rendering all the elements
 // in the html.
 
-// Display today's day and date
+ // Code that displays the current date in the header of the page.
 var currentDay = dayjs().format('MMM D, YYYY');
 $(document).ready(function () {
 
@@ -16,15 +16,15 @@ $(document).ready(function () {
         // Save text in local storage
         localStorage.setItem(time, text);
     })
-           
+   
     function timeTracker() {
     //get current number of hours.
-        var timeNow = moment().hour();
-        
+        var timeNow = moment().hours();
+        console.log(timeNow)
         // loop over time blocks
         $(".time-block").each(function () {
-        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
-        
+        var blockTime = parseInt($(this).attr("id").split("-")[1]);
+        console.log(blockTime)
         // To check the time and add the classes for background indicators
         if (blockTime < timeNow) {
         $(this).removeClass("future");
@@ -46,18 +46,18 @@ $(document).ready(function () {
     }
         
         // Get item from local storage if any
-        $("#hour8 .description").val(localStorage.getItem("hour8"));
-        $("#hour9 .description").val(localStorage.getItem("hour9"));
-        $("#hour10 .description").val(localStorage.getItem("hour10"));
-        $("#hour11 .description").val(localStorage.getItem("hour11"));
-        $("#hour12 .description").val(localStorage.getItem("hour12"));
-        $("#hour13 .description").val(localStorage.getItem("hour13"));
-        $("#hour14 .description").val(localStorage.getItem("hour14"));
-        $("#hour15 .description").val(localStorage.getItem("hour15"));
-        $("#hour16 .description").val(localStorage.getItem("hour16"));
-        $("#hour17 .description").val(localStorage.getItem("hour17"));
+        $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+        $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+        $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+        $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+        $("#hour-13 .description").val(localStorage.getItem("hour-13"));
+        $("#hour-14 .description").val(localStorage.getItem("hour-14"));
+        $("#hour-15 .description").val(localStorage.getItem("hour-15"));
+        $("#hour-16 .description").val(localStorage.getItem("hour-16"));
+        $("#hour-17 .description").val(localStorage.getItem("hour-17"));
         
         timeTracker();
+    })
     // TODO: Add a listener for click events on the save button. This code should
     // use the id in the containing time-block as a key to save the user input in
     // local storage. HINT: What does `this` reference in the click listener
@@ -75,8 +75,3 @@ $(document).ready(function () {
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
-
-    
-  })
